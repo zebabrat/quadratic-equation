097335196ada5179e2610b7c9b607fbcfdbd295f
Андрей Забавко <zeba.brat@Zebabrat.local>
2018-09-13 18:18:23 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  const arrayOfCoefficient = equation.split(' ');
+  let arrayOfRoots = [];
+
+  const a = Number(arrayOfCoefficient[0]);
+  const b = Number(arrayOfCoefficient[3] + arrayOfCoefficient[4]);
+  const c = Number(arrayOfCoefficient[7] + arrayOfCoefficient[8]);
+
+  const discriminant = (b * b) - 4 * a * c;
+
+  const rootFirst = Math.round(((-b) + Math.sqrt(discriminant)) / (2 * a));
+  const rootSecond = Math.round(((-b) - Math.sqrt(discriminant)) / (2 * a));
+  
+  arrayOfRoots.push(rootFirst, rootSecond);
+  arrayOfRoots.sort((a, b) => a - b);
+
+  return arrayOfRoots
 }
